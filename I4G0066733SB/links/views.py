00f5df2edba5b77a5c9e657d5eede8f35f7f89a5
macanotmarker <macasorekingdavid@gmail.com>
2022-07-21 00:57:16 +0100

    added the remaining files

--- I4G0066733SB/links/views.py
+++ I4G0066733SB/links/views.py
@@ -1,7 +1,15 @@
+import datetime
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from .serializer import LinkSerializer
 from links.models import Link
+from django.utils import timezone
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import models
+from . import serializer
 
 class PostListApi(GenericAPIView):
     queryset = Link.objects.filter(active=True)
@@ -23,6 +31,33 @@ class PostUpdateApi(GenericAPIView):
 class PostDeleteApi(GenericAPIView):
     queryset = Link.objects.filter(active=True)
     serializer_class = LinkSerializer
+
+class ActiveLinkView(APIView):
+    """
+    Returns a list of all active (publicly accessible) links
+    """
+
+    def get(self, request):
+        """
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        qs = models.Link.public.all()
+        data = serializer.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
+class RecentLinkView(APIView):
+    """
+    Returns a list of recently created active links
+    """
+
+    def get(self, request):
+        """
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        qs = models.Link.public.filter(created_date__gte=seven_days_ago)
+        data = serializer.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
 
 
 
