953118e7a507a460177fd7acc5e17de1ed18f2c2
macanotmarker <macasorekingdavid@gmail.com>
2022-07-21 00:37:38 +0100

    added files from last api repo

---
+++ I4G0066733SB/links/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializer import LinkSerializer
+from links.models import Link
+
+class PostListApi(GenericAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostCreateApi(GenericAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+    
+
+class PostDetailApi(GenericAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi(GenericAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteApi(GenericAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+
